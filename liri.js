86a7b5b0aab1ec8076f4b003a1e786eeb1449c6b
Jon Finley <lilfin19@gmail.com>
2019-10-04 12:44:28 -0500

    formating of spotify's responses added

--- liri.js
+++ liri.js
@@ -3,33 +3,30 @@ require('dotenv').config();
 
 // Make it so liri.js can take in one of the following commands:
 var keys= require("./keys.js")
-// * `concert-this`
-// var fs= require("fs")
-// // * `spotify-this-song`
-// var axios= require("axios");
-// * `movie-this`
-
-// * `do-what-it-says`
-
 
 var Spotify = require('node-spotify-api');
 // var spotify = new Spotify(keys.spotify)
 var spotify = new Spotify(keys.spotify);
 
-console.log(keys)
- 
-// var spotify = new Spotify({
-//   id: <your spotify client id>,
-//   secret: <your spotify client secret></your>
-// });
- 
+function runspot(){
 spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
   if (err) {
     return console.log('Error occurred: ' + err);
   }
  
-console.log(JSON.stringify(data, null, 2));
+// console.log(JSON.stringify(data.tracks.items[0], null, 2));
+// console.log(data.tracks.items[0].album.artists[0]);
+console.log("link to preview song   :   " +data.tracks.items[0].album.artists[0].external_urls.spotify);
+console.log("this song's artist is  :   " + data.tracks.items[0].album.artists[0].name);
+console.log("this song's name is    :   "+data.tracks.items[0].name);
+console.log("this song's album name :   "+ data.tracks.items[0].album.name);
 });
+ }
+runspot();
+
+
+
+
 //   spotify.search({ type: 'track', query: 'All the Small Things' }, function(err, data) {
 //     if (err) {
 //       return console.log('Error occurred: ' + err);
